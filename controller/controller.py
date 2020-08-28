b1cab001694cc05af892c2dc178f9c232a7a76cc
Alice <alice.bord1@gmail.com>
2020-08-28 14:52:46 +0200

    Allowed the snake to move and to eat a snack. Each time a key from the keyboard is pressed, the controller calls the move function of the Snake object in order to move it : the move function moves each body part from the end to the beginning of the snake (the last part of the snake is treated first and the first part is treated lastly). The head is moved according to the direction taken (x and y offsets (move_ip)), and the other body parts are moved according to each previous body part of a current body part (pos x and pos y). Each time the snake eats a snack, a new body part is created and added to it, then, a new random snack is generated and drew on the surface.

--- controller/controller.py
+++ controller/controller.py
@@ -5,6 +5,7 @@ sys.path.append("..\\view\\")
 sys.path.append("..\\model\\")
 from view import SnakeInterface
 from Snake import Snake
+from Snack import Snack
 
 #pdb.set_trace()
 
@@ -20,8 +21,10 @@ class Controller:
     def __init__(self):
         # Init the game.
         self.interface = SnakeInterface(width, width, blockSize, self)
-        self.snake = Snake(self, 5)
+        self.snake = Snake(self, 10, (0,0,255))
         self.snakeBody = self.snake.getBody()
+        self.snack = Snack(self)
+        self.snack.setPos() # We ask a new position for the snack.
         #print(self.snakeBody)
 
     """
@@ -44,10 +47,15 @@ class Controller:
         # Positions.
         x = 0
         y = 0
+        # Will be used to store the new direction.
+        newDirection = None
+        # Will be used to store the previous direction in order for the snake to not be allowed to go back on himself.
         direction = None
-        # speed
+        # The speed of the snake.
         speed = self.snake.speed
-
+        # The color of the snake.
+        snakeColor = self.snake.getColor()
+        
         # Entering an infinite loop (necessary in order to display the game continually as long as we don't ask to quit the game).
         while Flag:
             # Allowing the possibility to quit the game
@@ -56,25 +64,41 @@ class Controller:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_RIGHT and direction != pygame.K_LEFT:
                         x = blockSize
                         y = 0
-                        direction = pygame.K_RIGHT
-                    elif event.key == pygame.K_DOWN:
+                        # Storing the new direction.
+                        newDirection = pygame.K_RIGHT
+                    elif event.key == pygame.K_DOWN and direction != pygame.K_UP:
                         x = 0
                         y = blockSize
-                        direction = pygame.K_DOWN
-                    elif event.key == pygame.K_UP:
+                        # Storing the new direction.
+                        newDirection = pygame.K_DOWN
+                    elif event.key == pygame.K_UP and direction != pygame.K_DOWN:
                         x = 0
                         y = - blockSize
-                        direction = pygame.K_UP
-                    elif event.key == pygame.K_LEFT:
+                        # Storing the new direction.
+                        newDirection = pygame.K_UP
+                    elif event.key == pygame.K_LEFT and direction != pygame.K_RIGHT:
                         x = - blockSize
                         y = 0
-                        direction = pygame.K_LEFT
+                        # Storing the new direction.
+                        newDirection = pygame.K_LEFT
+                    # NB : We didn't update the newDirection variable here (which could let think that it's redundant)
+                        # but it was on purpose because if we did it such thing, the snake could have been able to go back on himself
+                        # after having pressed the same key twice.
+                    # Storing the direction which will become the previous one at the next iteration of the loop.
+                    direction = newDirection
 
             # Moving the snake
-            self.snake.move(x, y, direction)
+            self.snake.move(x, y, snakeColor, newDirection)
+            ateSnack = self.compareSnackAndSnakePos()
+            if ateSnack:
+                #print("x = ", x, " ; y = ", y)
+                # We create a new body part for the snake.
+                self.snake.createBody(snakeColor, False, x, y)
+                # We generate a new snack position.
+                self.snack.setPos()
             # Updating the interface of the game.
             pygame.display.update()
             clock.tick(speed)
@@ -130,6 +154,49 @@ class Controller:
     """
     def fillSurface(self, color, rectObject = None):
         self.interface.fillSurface(color, rectObject)
+
+
+    """
+    Call the drawRect function if the position of the snack is not the same as the snake. Otherwise, a new positin for the snack is asked. 
+    
+    :param int x: Abscissa.
+    :param int y: Ordinate.
+    :param tuple(int, int, int) color: Color of the rectObject ((R, G, B) format).
+    :param Rect rectObject: Rect object to be drawn (the snack). 
+    """
+    def drawSnack(self, x, y, color, rectObject):
+        # Declaration.
+        snackOnSnake = self.compareSnackAndSnakePos()
+
+        if snackOnSnake:
+            self.snack.setPos()
+        else:
+            self.drawRect(x, y, color, rectObject)
+    
+
+    """
+    Compare the position of the snake and the position of the snack. 
+    
+    :return: True if they are at the same position, else False.
+    """
+    def compareSnackAndSnakePos(self):
+        # Declaration.
+        snakeBody = self.snakeBody
+        snackPos_x = self.snack.getPos_x()
+        #print("snack pos x : ", snackPos_x)
+        snackPos_y = self.snack.getPos_y()
+        #print("snack pos y : ", snackPos_y)
+        isAtTheSamePos = False # Init the boolean to false in order to test.
+
+        # We browse all the items of the snake.
+        for key, value in snakeBody.items():
+            #print("snake pos x : ", snakeBody[key].x)
+            #print("snake pos y : ", snakeBody[key].y, "\n")
+            # If an item is placed at the same place of the snack:
+            if snakeBody[key].x - 1 == snackPos_x and snakeBody[key].y - 1 == snackPos_y :
+                # We change the boolean var to true.
+                isAtTheSamePos = True
+        return isAtTheSamePos
 
 
 if __name__ == "__main__":
--- model/Snake.py
+++ model/Snake.py
@@ -4,6 +4,8 @@ import pdb
 from random import randrange
 sys.path.append("..\\controller\\")
 
+#pdb.set_trace()
+
 
 class Snake:
     """
@@ -12,15 +14,17 @@ class Snake:
     :param Controller controller: The current controller.
     :param int speed: The speed of the snake.
     """
-    def __init__(self, controller, speed):
+    def __init__(self, controller, speed, color):
         # Storing the controller.
         self.controller = controller
+        # Storing the color.
+        self.color = color
         # Creating the dictionary for the body.
         self.body = {}
-        # Adding the head to the body.
-        self.createHead((0,0,255))
         # Used as key to add new body parts to the dict body & used as a counter (score).
         self.countBody = 0
+        # Adding the head to the body.
+        self.createHead(self.color)
         self.speed = speed
 
 
@@ -34,6 +38,15 @@ class Snake:
 
 
     """
+    Color getter.
+    
+    :return: The color property of the Snake object. 
+    """
+    def getColor(self):
+        return self.color
+
+
+    """
     Redirect to the createBody function with True given in parameter to say : we wannna create & store the head of the snake's body.
     
     param tuple(int, int, int) color: The color used to create the head of the snake. 
@@ -43,64 +56,124 @@ class Snake:
 
 
     """
-    Create a rectangle -> actually call the controller which will call the view to create the rectangle. 
+    Create a new body part of the snake and add it to the dict containing all the body parts. 
     
-    :param tuple(int, int, int) color: The color used to create the body. 
-    :param boolean isHead: (Optional) If passed, the head will be created and stored, otherwise a body part will be created. 
+    param tuple(int, int, int) color: The color used to create the body part. 
+    param boolean isHead: (Optional) True if it's the head of the snake which is to be created, False otherwise. 
+    param int x: (Optional) The pos x of the new body part (randomly generated inside the function if it's the head which is to be created).
+    param int y: (Optional) The pos y of the new body part (randomly generated inside the function if it's the head which is to be created).
     """
-    def createBody(self, color, isHead = False):
+    def createBody(self, color, isHead = False, x = None, y = None):
         # Saving the block size.
         blockSize = self.controller.getBlockSize()
         width = self.controller.getWidth()
         height = self.controller.getHeight()
-        # Generate random position.
-        x = randrange(0, width, blockSize) # to remember : param randrange(begin, max, step) (we go 20 by 20 so that the square will always be visible in a grid' square)
-        y = randrange(0, height, blockSize)
-        # We only create the container to be transfered to the view via the controller, so that we store the container into the dictionary.
-        rectObject = pygame.Rect(x + 1, y + 1, blockSize - 2, blockSize - 2)
-        #print("x : ", x, " ; y : ", y)
+
+        # print("x : ", x, " ; y : ", y)
+        #print("isHead = ", isHead)
 
         # If it is the head to be created & stored :
         if isHead:
-            # We add the value x, y and rect for a key called "Head".
-            self.body["Head"] = rectObject
+            # Generate random position.
+            x = randrange(0, width, blockSize)  # to remember : param randrange(begin, max, step) (we go 20 by 20 so that the square will always be visible in a grid' square)
+            y = randrange(0, height, blockSize)
+            rectObject = pygame.Rect(x + 1, y + 1, blockSize - 2, blockSize - 2)
+            # We add the rect for a key called "Head".
+            self.body[self.countBody] = rectObject
+            # print(self.body)
+        else:
+            # Else, it's a body part that is to be created.
+            # Incrementation of the counter.
+            self.countBody += 1
+            x = self.body[self.countBody - 1].x - 1 - x
+            y = self.body[self.countBody - 1].y - 1 - y
+            # Creation of a rect object (the new body part).
+            rectObject = pygame.Rect(x + 1, y + 1, blockSize - 2, blockSize - 2)
+            # We add the rect to "body" (the key is the counter).
+            self.body[self.countBody] = rectObject
             #print(self.body)
+
         # Call the controller to ask for the creation of a cube visible on the interface given the position, the color and if it's the head of the body or not.
         self.controller.drawRect(rectObject.x, rectObject.y, color, rectObject, isHead)
 
 
-    '''
-    To move the snake.
-     
-    :param event.type direction: (Optional) The direction the snake took. 
-    '''
-    def move(self, x, y, direction = None):
-        # Declaration.
+    """
+    Move the snake.
+    Each pos x and pos y of each body part of the snake is moved:
+    The head of the snake is moved depending on the x and y offsets.
+    For all the other body parts : the posx and posy of the body part become posx and posy of the previous body part. 
+    
+    param int x: The x offset (0, 20, or -20).
+    param int y: The y offset (0, 20, or -20).
+    param tuple(int, int, int) color: The color of the snake.
+    param event.key direction: (Optional) The direction taken by the snake.
+    """
+    def move(self, x, y, color, direction=None):
+        # Declarations.
         width = self.controller.getWidth()
         height = self.controller.getHeight()
 
-        for key, value in self.body.items():
+        #print("x = ", x, " ; y = ", y)
+        # We browse the dict from its end to its beginning.
+        for key in range(len(self.body) - 1, -1, -1):
+            # Storing the current Rect object in a variable.
             rect = self.body[key]
             # We fill the current rect container with a black color to show the movement of the snake.
-            self.controller.fillSurface((0,0,0), rect)
-
-            if rect.x + x > width:
-                # We're moving to the right (we return to the left)
-                rect.move_ip(- width, y)
-            elif rect.x + x <= 0:
-                # We're moving to the left (we return to the right)
-                rect.move_ip(width, y)
-            if rect.y + y > height:
-                # We're moving down (we return to the top)
-                rect.move_ip(x, - height + y)
-            elif rect.y + y <= 0:
-                # We're moving up (we return to the bottom)
-                rect.move_ip(x, height + y)
+            self.controller.fillSurface((0, 0, 0), rect)
+            # If the rect is the head of the snake:
+            if key == 0:
+                # Testing the next position of the head to manage the edges of the surface.
+                if rect.x + x > width:
+                    # We're reaching the right edge (we return to the left)
+                    rect.move_ip(- width, y)
+                elif rect.x + x <= 0:
+                    # We're reaching the left edge (we return to the right)
+                    rect.move_ip(width, y)
+                if rect.y + y > height:
+                    # We're reaching the lower edge (we return to the top)
+                    rect.move_ip(x, - height + y)
+                elif rect.y + y <= 0:
+                    # We're reaching the upper edge (we return to the lower edge)
+                    rect.move_ip(x, height + y)
+                else:
+                    # We continue moving as expected
+                    rect.move_ip(x, y)
+                #print("new head rect.x : ", rect.x)
+                #print("new head rect.y : ", rect.y, "\n")
             else:
-                # We continue moving as expected
-                rect.move_ip(x, y)
+                # We store the previous Rect object.
+                prevRect = self.body[key - 1]
+                '''print("dict : ", self.body,"\n")
+                print("rect[",key,"] : ", rect)
+                print("rect.x : ", rect.x)
+                print("rect.y : ", rect.y,"\n")
+                print("prevRect[",key - 1,"] : ", prevRect)
+                print("prevRect.x : ", prevRect.x)
+                print("prevRect.y : ", prevRect.y,"\n")'''
+                # The current x pos of the rect become the x pos of the previous rect.
+                rect.x = prevRect.x
+                # The current y pos of the rect become the y pos of the previous rect.
+                rect.y = prevRect.y
+                '''print("new rect.x : ", rect.x)
+                print("new rect.y : ", rect.y,"\n")'''
+        # We ask the controller to move all the rects.
+        self.drawRects(self.body, color, direction)
 
-            # We ask the controller to ask the view to move the rect (drawing the rect moved thanks to "move_ip" actually).
-            self.controller.drawRect(rect.x, rect.y, (0,0,255), rect, key == "Head", direction)
+
+    """
+    Browse the dict passed as an arg and for each item (rect objects), the controller ask the view to draw the rect object according to its pos x and pos y.
+    
+    param Dictionary<Rect> dict: A dictionary containing all the rect objects of the snake.
+    param tuple(int, int, int) color: The color of the snake. 
+    param event.key direction: The direction taken by the snake. 
+    """
+    def drawRects(self, dict, color, direction):
+        # Browsing the dictionary.
+        for key, value in dict.items():
+            # Storing the current item.
+            rect = dict[key]
+            # Calling the controller to draw the rect on the surface (by calling the view).
+            self.controller.drawRect(rect.x, rect.y, color, rect, key == 0, direction)
+
 
 
